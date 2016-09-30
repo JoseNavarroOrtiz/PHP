a957beaf235548d21b1bc4f954dfdb24c04efa58
JoseNavarroOrtiz <ortiznavarrojose@live.com>
2016-09-30 10:32:31 +0200

    Subir Relacion de Ejercicios 1

---
+++ Relacion1/ejer3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <table>
+        <?php
+        
+            echo"<tr>";
+               echo "<td>" ."gato". "</td>";
+               echo "<td>" ."cat". "</td>";
+           echo "</tr>";
+           echo"<tr>";
+               echo "<td>" ."perro". "</td>";
+               echo "<td>" ."dog". "</td>";
+           echo "</tr>";
+        ?>
+        </table>
+    </body>
+</html>
---
+++ Relacion1/ejer4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $pNumero= 144;
+        $sNumero= 999;
+            echo "suma= ",$pNumero+$sNumero,"<br>" ;
+            echo "resta=",$pNumero-$sNumero,"<br>";
+            echo "multiplicacion=",$pNumero*$sNumero,"<br>";
+            echo "division=",$pNumero/$sNumero,"<br>";
+           
+            
+        ?>
+    </body>
+</html>
